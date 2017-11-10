0cb84e92864c0b76bac8f1d47a620f4a47817c00
Andrei <andrei.papay@tmacomms.com>
2017-11-10 11:32:08 +1100

    wip

--- conf/nifiserver.sh
+++ conf/nifiserver.sh
@@ -2,5 +2,6 @@
 # `/sbin/setuser memcache` runs the given command as the user `memcache`.
 # If you omit that part, the command will be run as root.
 #exec /nifi/bin/nifi.sh run
-exec tail -F /nifi/logs/nifi-app.log & /nifi/bin/nifi.sh run
+exec /opt/nifi/nifi-1.4.0/bin/nifi-env.sh
+exec tail -F /nifi/logs/nifi-app.log & /opt/nifi/nifi-1.4.0/bin/nifi.sh run
 #exec /sbin/setuser memcache /usr/bin/memcached >>/var/log/memcached.log 2>&1
