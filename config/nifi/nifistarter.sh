886569fed74b14fe9db6018f4c60b7e601744a6c
Andrei Papay <andrei.papay@tmacomms.com>
2017-12-01 03:19:32 +1100

    aa

--- config/nifi/nifistarter.sh
+++ config/nifi/nifistarter.sh
@@ -3,7 +3,7 @@
 export NIFI_HOME=/opt/nifi 
 $NIFI_HOME/bin/nifi-env.sh
 echo "Resetting owner on /opt/nifi:
-sudo chmod -R 0777 /opt/nifi/conf
+sudo chmod -R 0777 /opt/nifi
 sudo chown -R nifi:nifi /opt/nifi/
 echo "Resetting owner on /opt/nifi done"
 tail -F $NIFI_HOME/logs/nifi-app.log &  $NIFI_HOME/bin/nifi.sh run
