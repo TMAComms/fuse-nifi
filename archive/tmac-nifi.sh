fef988ab9eec9f312902906743c83b9a31b98422
Andrei <andrei.papay@tmacomms.com>
2018-02-21 18:09:57 +1100

    base

--- archive/tmac-nifi.sh
+++ archive/tmac-nifi.sh
@@ -22,7 +22,7 @@ echo "TMAC Nifi starter " ${NIFI_REGISTRYBASE}
 # `/sbin/setuser memcache` runs the given command as the user `memcache`.
 # If you omit that part, the command will be run as root.
 
-workifile="${NIFI_REGISTRYBASE}/conf/nifi-registry.properties"
+workifile="$NIFI_REGISTRYBASE/conf/nifi-registry.properties"
 if [ -f "$workifile" ]
 then
 	echo "$workifile found."
@@ -33,8 +33,6 @@ else
     cp -R /config/base/* $NIFI_REGISTRYBASE/conf/
 fi
 
-
-# always update ip and hosts 
 TMPHOSTIP=$(ip route get 1 | awk '{print $NF;exit}')
 echo "Container IP " ${TMPHOSTIP}
 echo "EVS Service DNS " ${EVS_SERVICEDNS}
@@ -43,15 +41,25 @@ echo "${TMPHOSTIP} ${EVS_SERVICEDNS}" >> /etc/hosts
 THISHOST=$(hostname -f)
 echo "${TMPHOSTIP} ${THISHOST}" >> /etc/hosts
 
-# always grabn latest jks backed into images
-echo "Update jks stores for " ${EVS_SERVICEDNS}
+
+echo "Update ssl config for " ${EVS_SERVICEDNS}
 #cp -f /tlskit/generated/${EVS_SERVICEDNS}/nifi.properties $NIFI_HOME/conf/nifi.properties
-cp -f /config/securitystores/truststore.jks $NIFI_REGISTRYBASE/conf/truststore.jks
-cp -f /config/securitystores/keystore.jks $NIFI_REGISTRYBASE/conf/keystore.jks
+cp -f /tlskit/generated/${EVS_SERVICEDNS}/truststore.jks $NIFI_REGISTRYBASE/conf/truststore.jks
+cp -f /tlskit/generated/${EVS_SERVICEDNS}/keystore.jks $NIFI_REGISTRYBASE/conf/keystore.jks
 echo "Update ssl config done"
 
 
-#sed -i "s~{EVS_SERVICEDNS}~${EVS_SERVICEDNS}~" $NIFI_REGISTRYBASE/conf/nifi-registry.properties
+sed -i "s~{EVS_SERVICEDNS}~${EVS_SERVICEDNS}~" $NIFI_REGISTRYBASE/conf/nifi-registry.properties
+
+
+
+echo "Update openid settings for " ${EVS_SERVICEDNS} 
+sed -i "s~{EVS_AUTHDISCOVERYURL}~${EVS_AUTHDISCOVERYURL}~" $NIFI_REGISTRYBASE/conf/nifi-registry.properties
+sed -i "s~{EVS_AUTHCLIENTID}~${EVS_AUTHCLIENTID}~" $NIFI_REGISTRYBASE/conf/nifi-registry.properties
+sed -i "s~{EVS_AUTHCLIENTSECRET}~${EVS_AUTHCLIENTSECRET}~" $NIFI_REGISTRYBASE/conf/nifi-registry.properties
+echo "Update openid settings completed " 
+
+#EVS_SERVICEDNS EVS_AUTHDISCOVERYURL
 
 
 echo "Setting up local ip " ${TMPHOSTIP}
@@ -59,6 +67,7 @@ echo "Setting up local ip " ${TMPHOSTIP}
 echo "TMAC Nifi registry - running up nifi " 
 #${NIFI_REGISTRYBASE}/scripts/start.sh
 
+/opt/nifiregistry/bin/nifi-registry.sh run 
 
 # Continuously provide logs so that 'docker logs' can    produce them
 tail -F "${NIFI_REGISTRYBASE}/logs/nifi-registry-app.log" &
