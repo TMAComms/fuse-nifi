bc59ff59c4154608c10511157f8197e844573509
Andrei Papay <andrei.papay@tmacomms.com>
2018-08-09 05:04:42 +1000

    m

--- build.ps1
+++ build.ps1
@@ -1,6 +1,8 @@
 $ErrorActionPreference = "Stop"
 
 $tmacBaseImage = "tmacomms/fuse-nifi:latest"
+$tmacRegistryBaseImage = "tmacomms/fuse-nifiregistry:latest"
+$tmacSchemaRegistryBaseImage = "tmacomms/fuse-schemaregistry:latest"
 $tmacRegistryPath = "registry.tmacomms.com"
 $tmacAzureRegistryPath = "tmacregistry-tmacomms.azurecr.io"
 $tmacFullPath = "$tmacAzureRegistryPath/$tmacBaseImage"
@@ -13,6 +15,12 @@ echo "Building image $tmacBaseImage"
 
 #docker build --file Dockerfile-jobserver.build --squash -t  tmacomms/evs-jobserver:latest .
 docker build --file Dockerfile --squash -t $tmacBaseImage .
+echo "Building image $tmacRegistryBaseImage"
+docker build --file Dockerfile.registry --squash -t $tmacRegistryBaseImage .
+
+echo "Building image $tmacSchemaRegistryBaseImage"
+docker build --file Dockerfile.schemaregistry --squash -t $tmacSchemaRegistryBaseImage .
+
 
 echo "docker run -it --name nifi --rm -v ${pwd}/working:/working -p 9090:9090 $tmacBaseImage"
 
