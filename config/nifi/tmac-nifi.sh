b70fa9c6e4576325f47b97441efb1406db5624fa
Andrei Papay <andrei.papay@tmacomms.com>
2018-08-26 17:38:55 +1000

    fix startup

--- config/nifi/tmac-nifi.sh
+++ config/nifi/tmac-nifi.sh
@@ -81,5 +81,17 @@ echo "Setting up local ip " ${TMPHOSTIP}
 #export NIFI_WEB_HTTP_HOST=${TMPHOSTIP}
 
 echo "TMAC Nifi running up nifi " 
-${NIFI_BASE_DIR}/scripts/start.sh
+
+
+# Continuously provide logs so that 'docker logs' can    produce them
+tail -F "${NIFI_HOME}/logs/nifi-app.log" &
+"${NIFI_HOME}/bin/nifi.sh" run &
+nifi_pid="$!"
+
+trap "echo Received trapped signal, beginning shutdown...;" KILL TERM HUP INT EXIT;
+
+echo NiFi running with PID ${nifi_pid}.
+wait ${nifi_pid}
+
+#${NIFI_BASE_DIR}/scripts/start.sh
 
